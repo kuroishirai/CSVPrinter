c644ff85376db7f685fff9efd6391c912a131821
shirai tatsuya <haranokuroi@gmial.com>
2024-04-25 17:12:25 +0900

    second commit

--- tests/test_CSVPrinter.py
+++ tests/test_CSVPrinter.py
@@ -1,21 +1,24 @@
 import unittest
+import os
 from speciallecture.CSVPrinter import CSVPrinter
 
 class TestCSVPrinter(unittest.TestCase):
     def test_read_row(self):
-        printer = CSVPrinter("sample.csv")
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        printer = CSVPrinter(os.path.join(dir_path,"sample.csv"))
         l = printer.read()
         self.assertEqual(4,len(l))
 
 
     def test_read_col(self):
-        printer = CSVPrinter("sample.csv")
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        printer = CSVPrinter(os.path.join(dir_path,"sample.csv"))
         l = printer.read()
         self.assertEqual(2,len(l[0]))
 
-
     def test_read_error(self):
-        printer = CSVPrinter("saple.csv")
+        dir_path = os.path.dirname(os.path.abspath(__file__))
+        printer = CSVPrinter(os.path.join(dir_path,"saple.csv"))
         try:
             l = printer.read()
         except FileNotFoundError:
